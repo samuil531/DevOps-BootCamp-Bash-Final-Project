f09360749107c8cd0dbbe2cd4b43a8a3a68ecd2f
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-13 21:18:01 +0300

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -2,62 +2,75 @@
 
 currentVersion="1.23.0"
 
+#######################################
+# Uploads files to transfer.sh
+# Globals:
+# None
+# Arguments:
+#$@
+# Outputs:
+# Writes upload process and result info to stdout
+# Returns:
+# 1 error
+#######################################
+
+#Upload
 httpSingleUpload()
 {
-   response=$(curl -A curl -# --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
-   printUploadResponse
-}
-
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Transfer File URL: $response
-EOF
-}
-
-singleUpload()
-{
-  for i in "$@"
-  do
-    
-    #filePath=$(echo "$i" | sed 's/~/$HOME/g')
-    filePath=$i ; echo "${i//~/$HOME}"
-
-    if [ ! -e "$filePath" ]; then
-    {
-      echo "Error: invalid file path";
-      return 1;
-    };
-    fi
-
-    tempFileName=$(echo "$i" | sed "s/.*\///")
-    echo "Uploading $tempFileName"
-    httpSingleUpload "$filePath" "$tempFileName"
+  for i in "$@"; do
+    file=$i
+    echo -e " Uploading "$i""
+    response=$(curl --progress-bar --upload-file "$1" "https://transfer.sh/$file") || { echo -e "Failure!"; return 1;}
+    echo -e "Transfer File URL: "$response" \n" 
   done
 }
 
+#Download
 singleDownload()
 {
-  filePath=$(echo "$1" | sed 's/\.\///g')
-  
-  if [ ! -d "$filePath" ]
-  then
-    mkdir "$filePath"
-   # echo "create $filePath"	    
+  if [[ ! -d $2 ]];then 
+    echo "Creating missing directory..."
+    mkdir -p "$2/$3"
   fi
-  
-  echo "Downloading $3"
-  response=$(curl -# --url "https://transfer.sh/$2/$3" --output "$filePath/$3")
+  echo " Downloading "$4""
+  d_response=$(curl --progress-bar --create-dirs -o "$4" "https://transfer.sh/" --output-dir ./"$2"/"$3") || { echo -e " Failure!"; return 1;}
   printDownloadResponse
 }
 
-printDownloadResponse()
+#Prints
+printDownloadResponse() 
 {
-  fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Success! $fileID
-EOF
+  echo -e " Success!"
 }
 
-singleUpload "$1"
+#help
+help() 
+{
+ echo -e " Description: Bash tool to transfer files from the command line. 
+        Usage: 
+        -d  Download file from https://transfer.sh/{particular folder} 
+        -h  Show the help about attributes 
+	     -v  Get the tool version 
+    
+        Examples: 
+     ./transfer.sh test.txt               - upload single file	  
+	  ./transfer.sh test.txt test2.txt ... - upload multiple files	  
+	  ./transfer.sh -v                     - version
+	  ./transfer.sh -h                     - view help"
+}
+
+#main
+main() {
+  if [[ $1 == "-d" ]]; then
+    singleDownload "$@"
+  elif [[ $1 == "-v" ]]; then
+    echo "$currentVersion"
+  elif [[ $1 == "-h" ]]; then
+    help
+    else
+    upload "$@"
+  fi
+}
+
+#call main
+main "$@"

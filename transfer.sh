0ff23911d89382c2f135765d5effaab1766ee360
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-13 19:43:53 +0300

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,10 +1,11 @@
-#!/bin/curl
+#!/bin/bash
 
-currentVersion="0.0.1"
+currentVersion="1.23.0"
 
 httpSingleUpload()
 {
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+   response=$(curl -A curl -# --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+   printUploadResponse
 }
 
 printUploadResponse()
@@ -17,12 +18,46 @@ EOF
 
 singleUpload()
 {
-  filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if ! -f "$filePath" ;then { echo "Error: invalid file path"; return 1;}; fi
-  tempFileName=$(echo "$1" | sed "s/.*\///")
-  echo "Uploading $tempFileName"
-  httpSingleUpload "$filePath $tempFileName"
+  for i in "$@"
+  do
+    
+    #filePath=$(echo "$i" | sed 's/~/$HOME/g')
+    filePath=$i ; echo "${i//~/$HOME}"
+
+    if [ ! -e "$filePath" ]; then
+    {
+      echo "Error: invalid file path";
+      return 1;
+    };
+    fi
+
+    tempFileName=$(echo "$i" | sed "s/.*\///")
+    echo "Uploading $tempFileName"
+    httpSingleUpload "$filePath" "$tempFileName"
+  done
 }
 
-singleUpload "$1" || exit 1
-printUploadResponse
+singleDownload()
+{
+  filePath=$(echo "$1" | sed 's/\.\///g')
+  
+  if [ ! -d "$filePath" ]
+  then
+    mkdir "$filePath"
+   # echo "create $filePath"	    
+  fi
+  
+  echo "Downloading $3"
+  response=$(curl -# --url "https://transfer.sh/$2/$3" --output "$filePath/$3")
+  printDownloadResponse
+}
+
+printDownloadResponse()
+{
+  fileID=$(echo "$response" | cut -d "/" -f 4)
+  cat <<EOF
+Success! $fileID
+EOF
+}
+
+singleUpload "$1"
